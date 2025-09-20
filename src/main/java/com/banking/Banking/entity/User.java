3a27aa2327555ebe762e162af228d81c9ddcdfb0
Alperen Çelebi <alperen.v.celebi44@gmail.com>
2025-09-20 14:36:29 +0300

    add Entity package Role && User

---
+++ src/main/java/com/banking/Banking/entity/User.java
@@ -0,0 +1,49 @@
+package com.banking.Banking.entity;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+	
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    @Column(unique = true, nullable = false)
+	    private String username; // sub
+
+	    @Column(nullable = false)
+	    private String password; // bcrypt
+
+	    @ManyToMany(fetch = FetchType.EAGER)
+	    @JoinTable(name = "user_roles",
+	       joinColumns = @JoinColumn(name = "user_id"),
+	       inverseJoinColumns = @JoinColumn(name = "role_id"))
+	    private Set<Role> roles = new HashSet<>();
+
+	    private boolean enabled = true;
+	    private Instant createdAt = Instant.now();
+	}
+
