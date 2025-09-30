eeb20016b19ce46610b755162d8adbb4cee9e505
Alperen Çelebi <alperen.v.celebi44@gmail.com>
2025-09-30 18:55:21 +0300

    Add ErrorResponse.java GlobalExceptionHandler.java

---
+++ src/main/java/com/banking/Banking/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.banking.Banking.exception;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+	@AllArgsConstructor
+	public class ErrorResponse {
+
+	    private String errorCode;
+	    private String message;
+	    private String details;  // opsiyonel, debug için
+	}
+	
